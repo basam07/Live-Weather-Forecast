a4f15a02d5a3d37bd646e5a5d6d6c1b24a6a9f37
basam07 <mbasamfarooq@gmail.com>
2023-11-15 21:47:38 +0500

    second commit

--- weather.js
+++ weather.js
@@ -45,3 +45,16 @@ searchbtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 })
 checkWeather();
+
+var input = document.getElementById("input");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btn").click();
+  }
+});
